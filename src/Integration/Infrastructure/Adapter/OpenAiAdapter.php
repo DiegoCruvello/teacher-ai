b361be4b8520797c2afc078afd641e595bf3f736
Diego Cruvello <diego.cruvello@gmail.com>
2024-06-01 13:22:25 -0300

    fix: att prompt

--- src/Integration/Infrastructure/Adapter/OpenAiAdapter.php
+++ src/Integration/Infrastructure/Adapter/OpenAiAdapter.php
@@ -34,8 +34,10 @@ class OpenAiAdapter implements SendImageAdapterInterface
                             de acordo com o GTO. Forneça sua avaliação apenas sobre a qualidade da jogada. Ao final,
                             dê-me a melhor sugestão para ser usada no Flop, Turn e River. Retorne um json e cada índice deve ser
                             o flop, river e turn. Indíces adionais também podem ser criados para exibir mensagens para o usuário.
-                            Não precisa exemplificar que é um json, somente retornar um json com chave valor, somente.
-                            Se atente que o flop sempre são as 3 primeiras cartas da esquerda para a direita.'
+                            Não precisa exemplificar que é um json, somente retornar um json com chave valor, somente,
+                            Você vai fazer a leitura sempre da esquerda para a direita, o flop você vai mandar como as
+                            3 primeiras cartas, turn sempre a quarta e River a quinta carta, lembre que as cartas são coloridas
+                            ou preto em branco, que vai de A,2,3,4,5,6,7,8,9,10,J,Q,K.'
                         ],
                         [
                             'type' => 'image_url',
